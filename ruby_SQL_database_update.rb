76a2ee0679ef921c3b63fa2d1ab22f2176897cce
ryoux <s162951@stud.hioa.no>
2015-02-24 16:02:46 +0100

    Create ruby_SQL_database_update.rb

---
+++ ruby_SQL_database_update.rb
@@ -0,0 +1,25 @@
+#YouTube-link: https://www.youtube.com/watch?v=A_iYYft3SX8
+#Slik kjører man spørringene til My-SQL-databasen med Ruby:
+#Først oppretter man forbindelsen igjen:
+
+require 'rubygems'
+require 'dbi'
+
+#Oppretter forbindelsen til MySQL-serveren
+dbh = DBI.connect('DBI:Mysql:calendar_app','root','passordet')
+if dbh
+	puts "Databaseforbindelse opprettet."
+else
+	puts "Kunne ikke opprette databaseforbindelse, sjekk at parameterne er riktige/gyldige."
+end
+
+#Her skriver man selve spørringene, for eksempel vil man 
+#oppdatere e-mail adressen til en bruker som ønsker dette:
+
+sth = dbh.prepare(UPDATE deltagere SET email = 'ny@emailadresse.com'
+WHERE fornavn = 'Mario' AND etternavn = 'Super')
+
+#Så kjører man til slutt spørringen slik:
+
+sth.execute
+dbh.disconnect
